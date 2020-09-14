60c0ed96e02e555f022453b4f712c208d9e0f385
Wellington Leone <wellingtonleone@gmail.com>
2020-09-14 18:14:22 -0300

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
+  resources :blogs
+
+  resources :guides
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
+end
